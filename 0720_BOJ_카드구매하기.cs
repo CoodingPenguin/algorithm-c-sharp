f59b0d3f37b371400296d1d43b045e55cb581d60
CoodingPenguin <unodostre@naver.com>
2019-07-21 18:26:54 +0900

    2019년 7월 20일 알고리즘 : DP에 대한 감이 잡힐 듯 말 듯! 한 번 블로그에 정리할 것!

---
+++ 0720_BOJ_카드구매하기.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        static int MAXCARD; // 최대 살 수 있는 카드
+        static int MAXKIND; // 최대 고를 수 있는 카드 종류
+
+        static void Main()
+        {
+            /*--------------------입력--------------------*/
+            MAXCARD = int.Parse(Console.ReadLine());
+            //Console.WriteLine("디버깅 : " + MAXCARD);
+
+            string[] parsing = Console.ReadLine().Split(' ');
+            MAXKIND = parsing.Length;
+
+            int[] price = new int[MAXKIND];
+            for (int i = 0; i < MAXKIND; i++)
+            {
+                price[i] = int.Parse(parsing[i]);
+                //Console.WriteLine("디버깅[" + i + "] : " + price[i]);
+            }
+            /*--------------------------------------------*/
+
+            int[,] DpTable = new int[MAXKIND + 1, MAXCARD + 1];
+
+            for(int kindCnt = 0; kindCnt <= MAXKIND; kindCnt++)
+            {
+                for(int cardCnt = 0; cardCnt <= MAXCARD; cardCnt++)
+                {
+                    if (kindCnt == 0 || cardCnt == 0)
+                        DpTable[kindCnt, cardCnt] = 0;
+                    else if (cardCnt >= kindCnt)
+                        // 넣을 수 있다면
+                        // "그 전 종류에서 가능했던 조합"과 "그 전 종류에서 넣으려는 카드를 제외하고 가능했던 조합 + 넣으려는 카드" 중 가장 큰 것
+                        DpTable[kindCnt, cardCnt] = Math.Max(DpTable[kindCnt - 1, cardCnt], (DpTable[kindCnt, cardCnt - kindCnt] + price[kindCnt - 1]));
+                    else
+                        // 넣을 수 없다면
+                        // 그 전 조합을 넣는다
+                        DpTable[kindCnt, cardCnt] = DpTable[kindCnt - 1, cardCnt];
+
+                    //Console.WriteLine("DpTable[" + kindCnt+ ", " + cardCnt + "] : " +  DpTable[kindCnt, cardCnt]);
+                }
+                //Console.WriteLine("////////");
+            }
+
+            /*
+            for(int i = 0; i <= MAXKIND; i++)
+            {
+                for(int j = 0; j <= MAXCARD; j++)
+                {
+                    Console.Write(DpTable[i, j]);
+                }
+                Console.WriteLine();
+            }
+            */
+
+            Console.WriteLine(DpTable[MAXKIND, MAXCARD]);
+        }
+
+    }
+}
+ 
--- README.md
+++ README.md
@@ -5,4 +5,7 @@ C#으로 알고리즘 공부
 
 ## 2019.07.19
 - [ATM 문제](https://www.acmicpc.net/problem/11399) : 정렬 뒤 Weight를 주어서 구함
-- [카우버거알바생](https://www.acmicpc.net/problem/17208) : 아직 못 품
+- [카우버거알바생](https://www.acmicpc.net/problem/17208) : 아직 못 품 (KnapSack문제 응용)
+
+## 2019.07.20
+- [카드 구매하기](https://www.acmicpc.net/problem/11052) : 정통 DP문제! 많이 풀면서 적응해야할 듯ㅠㅠ
