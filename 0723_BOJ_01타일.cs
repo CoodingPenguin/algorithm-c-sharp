be5160a00450684048f8941d89758b1f4f27a64e
CoodingPenguin <unodostre@naver.com>
2019-07-23 15:06:12 +0900

    2019년 7월 23일 알고리즘 : 결국 피보나치 수열 문제 (DP를 하면 주로 런타임 에러가 많이 나는데 long으로 바꿔주거나 문제에서 나머지를 요구하면 아에 나머지 계산을 먼저 하자)

---
+++ 0723_BOJ_01타일.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            long[] result = new long[input + 1];
+            result[0] = 1;
+            result[1] = 1;
+            for(int i = 2; i <= input; i++)
+            {
+                result[i] = (result[i - 1] + result[i - 2]) % 15746;
+            }
+
+            Console.WriteLine(result[input]);
+        }
+    }
+}
+ 
