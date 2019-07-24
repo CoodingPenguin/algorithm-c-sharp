ae2d907db8ef2881089c3ebe72656737b02e43f5
CoodingPenguin <unodostre@naver.com>
2019-07-24 11:32:21 +0900

    2019년 7월 24일 알고리즘 : DP를 보면 들어야 하는 생각 2가지 - 점화식, 배열(테이블)

---
+++ 0724_BOJ_파도반수열.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfTest = int.Parse(Console.ReadLine());
+            int[] testCase = new int[numOfTest];
+            for(int i = 0; i < numOfTest; i++)
+            {
+                testCase[i] = int.Parse(Console.ReadLine());
+            }
+
+            int maxValue = MaxValue(testCase);
+            long[] arr = new long[maxValue + 1];
+            arr[0] = 0;
+            arr[1] = 1;
+            arr[2] = 1;
+            arr[3] = 1;
+            arr[4] = 2;
+            arr[5] = 2;
+
+            for(int i = 6; i <= maxValue; i++)
+            {
+                arr[i] = arr[i - 5] + arr[i - 1];
+            }
+
+            for(int i = 0; i < numOfTest; i++)
+            {
+                Console.WriteLine(arr[testCase[i]]);
+            }
+        }
+
+        static int MaxValue(int[] arr)
+        {
+            int max = 0;
+            for(int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > arr[max])
+                    max = i;
+            }
+
+            return arr[max];
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # algorithms
 C#으로 알고리즘 공부
+----
+## 나중에 다시 풀 문제들
+[카우버거알바생](https://www.acmicpc.net/problem/17208)  [개근상](https://www.acmicpc.net/problem/1563)
 
 ----
 
@@ -16,3 +19,9 @@ C#으로 알고리즘 공부
 ## 2019.07.22
 - [피보나치수2](https://www.acmicpc.net/problem/2748) : 배열을 이용한 중복 값 저장
 - [피보나치함수](https://www.acmicpc.net/problem/1003) : D&C의 단점을 설명해주는 것 같았지만 결국에는 DP문제
+
+## 2019.07.23
+- [01타일](https://www.acmicpc.net/problem/1904) : 결국 피보나치 수열 문제 (점화식을 세우다 보니 피보나치더라.. 이런식)
+
+## 2019.07.24
+- [파도반수열](https://www.acmicpc.net/problem/9461) : 점화식만 세우면 끝이다! 근데 런타임 에러가 자주 나네.. DP라서 그런가
