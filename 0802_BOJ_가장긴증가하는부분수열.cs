1fbb681c602233a5a0f22d2cacb5acf1431148b3
CoodingPenguin <unodostre@naver.com>
2019-08-12 12:50:26 +0900

    2019년 8월 2일 알고리즘 : 동적계획법에서 "아래의 값을 점차 누적해간다" 즉, bottom-up방식을 항상 고려할 것

---
+++ 0802_BOJ_가장긴증가하는부분수열.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main()
+        {
+            /*--------------입력--------------*/
+            int count = int.Parse(Console.ReadLine());
+
+            string[] parsing = Console.ReadLine().Split(' ');
+            int[] num = new int[count + 1];
+            num[0] = 0;
+            for(int i = 1; i <= count; i++)
+            {
+                num[i] = int.Parse(parsing[i - 1]);
+            }
+            /*--------------------------------*/
+
+            int[,] DpTable = new int[count + 1, count + 1];
+            
+            for(int row = 0; row < count; row++)
+            {
+                for(int col = row + 1; col <= count; col++)
+                {
+                    if (row == 0)
+                        DpTable[row, col] = 1;
+                    else if (num[row] >= num[col])
+                        DpTable[row, col] = 0;
+                    else if (num[row] < num[col])
+                    {
+                        int max = 0;
+                        for(int i = 0; i < row; i++)
+                        {
+                            if (DpTable[i, row] > max)
+                                max = DpTable[i, row];
+                        }
+                        DpTable[row, col] = max + 1;
+                    }
+                }
+            }
+
+            int maxCnt = 0;
+            for(int row = 0; row < count; row++)
+            {
+                for(int col = row + 1; col <= count; col++)
+                {
+                    if (DpTable[row, col] > maxCnt)
+                        maxCnt = DpTable[row, col];
+                }
+            }
+
+            Console.WriteLine(maxCnt);
+
+        }
+    }
+}
