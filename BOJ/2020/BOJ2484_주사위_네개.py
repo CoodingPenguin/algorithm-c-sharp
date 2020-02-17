aad4385d61b31099ec03f14bff2673971806104b
CoodingPenguin <unodostre@naver.com>
2020-02-17 17:54:14 +0900

    BOJ2484 주사위 네개

--- BOJ/2020/BOJ2484_주사위_네개.py
+++ BOJ/2020/BOJ2484_주사위_네개.py
@@ -1,5 +1,5 @@
 def calPrize(mydice):
-    sorted_dice = mydice.sort()
+    sorted_dice = sorted(mydice)
     count = len(set(sorted_dice))
 
     if count == 1:
@@ -7,16 +7,22 @@ def calPrize(mydice):
     elif count == 2:
         if sorted_dice[1] == sorted_dice[2]:
             return 10000 + sorted_dice[1] * 1000
-        return sorted_dice[1] * 500 + sorted_dice[2]*500
-    
-
-    
-
-ppl = int(input())
-dice = []
-
-for p in range(ppl):
-    lst = [int(x) for x in input().split()]
-    dice.append(lst)
+        return 2000 + sorted_dice[1] * 500 + sorted_dice[2]*500
+    elif count == 3:
+        if sorted_dice[0] == sorted_dice[1] or sorted_dice[1] == sorted_dice[2]:
+            return 1000 + sorted_dice[1] * 100
+        elif sorted_dice[2] == sorted_dice[3]:
+            return 1000 + sorted_dice[2] * 100
+    else:
+        return max(sorted_dice) * 100
 
 
+N = int(input())
+prizes = []
+
+for n in range(N):
+    prize = calPrize([int(x) for x in input().split()])
+    prizes.append(prize)
+
+
+print(max(prizes))
