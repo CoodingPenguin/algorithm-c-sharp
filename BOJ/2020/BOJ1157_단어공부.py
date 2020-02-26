395fa2955683afe4547153df1ab575338a9db837
CoodingPenguin <unodostre@naver.com>
2020-02-27 00:00:30 +0900

    BOJ1157 단어 공부

--- BOJ/2020/BOJ1157_단어공부.py
+++ BOJ/2020/BOJ1157_단어공부.py
@@ -1,2 +1,9 @@
 W = input().upper()
-WS = list(set(W))
+alphas = [0]*26
+for w in W:
+    alphas[ord(w) - ord('A')] += 1
+countSorted = sorted(alphas)
+if countSorted[-1] == countSorted[-2]:
+    print("?")
+else:
+    print(f"{chr(alphas.index(countSorted[-1])+ord('A'))}")
