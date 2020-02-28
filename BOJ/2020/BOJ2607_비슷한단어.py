89f2bae4d79060662091ca1081d37ab49407cb39
CoodingPenguin <unodostre@naver.com>
2020-02-29 01:50:34 +0900

    BOJ2607 비슷한 단어 하나 더할 때 로직 짜는 중

--- BOJ/2020/BOJ2607_비슷한단어.py
+++ BOJ/2020/BOJ2607_비슷한단어.py
@@ -1,13 +1,17 @@
 def result(t, cs):
     rt = 0
     for c in cs:
-        if t == c: rt += 1; continue;
+        if c == t:
+            rt += 1
+            continue
         # 하나를 더하거나
-        for ta, ca in t, c:
-            
+        if len(c) == len(t) + 1:
+            for i in range(len(t)):
+                if c[i] != t[i]:
+                    break
+
         # 하나를 빼거나
         # 하나의 문자를 다른 문자로 바꾸면
-
 
 
 N = int(input())
