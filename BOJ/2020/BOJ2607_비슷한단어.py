a4ab52e155cdad5b86fa090470eed6ff33705dfc
CoodingPenguin <unodostre@naver.com>
2020-03-22 03:26:40 +0900

    BOJ14681 사분면 고르기

--- BOJ/2020/BOJ2607_비슷한단어.py
+++ BOJ/2020/BOJ2607_비슷한단어.py
@@ -2,16 +2,30 @@ def result(t, cs):
     rt = 0
     for c in cs:
         if c == t:
+            print(f"Case #1  : {c}")
             rt += 1
-            continue
         # 하나를 더하거나
-        if len(c) == len(t) + 1:
-            for i in range(len(t)):
-                if c[i] != t[i]:
-                    break
-
+        elif len(c) == len(t) + 1 and set(c).issuperset(set(t)):
+            print(f"Case #2  : {c}")
+            rt += 1
         # 하나를 빼거나
+        elif len(c) == len(t) - 1 and set(c).issubset(set(t)):
+            print(f"Case #3  : {c}")
+            rt += 1
         # 하나의 문자를 다른 문자로 바꾸면
+        else:
+            if len(c) == len(t):
+                stack = t.copy()
+                for i in range(len(c)):
+                    try:
+                        stack.remove(c[i])
+                    except:
+                        pass
+                if len(stack) == 1:
+                    print(f"Case #4 : {c}")
+                    rt += 1
+
+    return rt
 
 
 N = int(input())
