31ec0931d924d1d2de0f9b13c240388080edb9b8
CoodingPenguin <unodostre@naver.com>
2020-04-16 01:04:36 +0900

    BOJ1712 손익분기점

---
+++ BOJ/2020/BOJ1712_손익분기점.py
@@ -0,0 +1,11 @@
+def result(a, b, c):
+    if b == c:
+        return -1
+    rt = a//(c-b)
+    if rt < 0:
+        return -1
+    return rt+1
+
+
+A, B, C = map(int, input().split())
+print(result(A, B, C))
