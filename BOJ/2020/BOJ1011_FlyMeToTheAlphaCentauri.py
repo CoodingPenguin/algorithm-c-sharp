9925f818057aa07eaa744a0d0cec9ea49e9eeb3a
CoodingPenguin <unodostre@naver.com>
2020-05-16 16:40:03 +0900

    BOJ1011 Fly me to the Alpha Centauri

--- BOJ/2020/BOJ1011_FlyMeToTheAlphaCentauri.py
+++ BOJ/2020/BOJ1011_FlyMeToTheAlphaCentauri.py
@@ -1,12 +1,16 @@
+import math
+
+
 def result(x, y):
     diff = y-x
-    if diff < 3:
-        return diff
-    rt, diff = 2, diff-1
-    while diff != 1:
-        diff //= 2
-        rt += 1
-    return rt
+    root = int(math.sqrt(diff))
+
+    if diff == root**2:
+        return 2*root-1
+    elif diff > (root**2 + (root+1)**2)//2:
+        return 2*root+1
+    else:
+        return 2*(root)
 
 
 for _ in range(int(input())):
