c8793283ab570fa3ccba91f82b5dc66a3de22a98
CoodingPenguin <unodostre@naver.com>
2020-08-18 18:21:27 +0900

    fix: 🐛 identation error

--- BOJ/2020/BOJ2231_분해합_1.py
+++ BOJ/2020/BOJ2231_분해합_1.py
@@ -1,12 +1,13 @@
 def smallest_constructor(n):
-      length = len(str(n))
-  if length == 1:
+    length = len(str(n))
+    if length == 1:
+        return 0
+    for i in range(9*(10**(length-2)), n):
+        decom = i + sum(map(int, list(str(i))))
+        if decom == n:
+            return i
     return 0
-  for i in range(9*(10**(length-2)), n):
-    decom = i + sum(map(int, list(str(i))))
-    if decom == n:
-      return i
-  return 0
+
 
 N = int(input())
 print(smallest_constructor(N))
