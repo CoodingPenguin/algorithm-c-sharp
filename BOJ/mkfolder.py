85d6051e0844ce7e5d7e93b05c331e57ac4d4a4f
CoodingPenguin <unodostre@naver.com>
2020-09-07 17:00:17 +0900

    Make BOJ folders per problem number 100

---
+++ BOJ/mkfolder.py
@@ -0,0 +1,10 @@
+import os
+
+for i in range(1000, 17300, 100):
+    try:
+        if not(os.path.isdir(f'{i} - {i+99}')):
+            os.makedirs(os.path.join(f'{i} - {i+99}'))
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            print('파일이 이미 존재합니다.')
+            raise
