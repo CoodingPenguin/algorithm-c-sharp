f0ada741c547d2d0588475e309fdc5a0e8f1dbea
CoodingPenguin <unodostre@naver.com>
2020-09-07 17:01:52 +0900

    fix: 🐛 fix typo in str

--- mkfolder.py
+++ mkfolder.py
@@ -6,5 +6,5 @@ for i in range(1000, 17300, 100):
             os.makedirs(os.path.join(f'{i} - {i+99}'))
     except OSError as e:
         if e.errno != errno.EEXIST:
-            print('파일이 이미 존재합니다.')
+            print('폴더가 이미 존재합니다.')
             raise
